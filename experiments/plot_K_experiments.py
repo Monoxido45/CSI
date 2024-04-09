7058f0a1cb6d71127ed46ade534e947139d75ee6
Monoxido45 <lucruz45.cab@gmail.com>
2024-04-09 16:17:05 -0300

    plotting optimal K

--- experiments/plot_K_experiments.py
+++ experiments/plot_K_experiments.py
@@ -11,7 +11,9 @@ palette = sns.color_palette(cc.glasbey, n_colors=12)
 
 # plotting data of interest
 
-# exponential empirical data
+# --------------------------------------------------
+# exponential experiment
+# --------------------------------------------------
 original_path = os.getcwd()
 data_path = original_path + "/experiments/results_data/experiment_K_exp.pkl"
 
@@ -19,9 +21,9 @@ obj_exp = pd.read_pickle(data_path)
 df_list = []
 # creating data frame list to concatenate
 for key in obj_exp.keys():
-  data = obj_exp[key]
-  data = data.assign(K = str(key))
-  df_list.append(data)
+    data = obj_exp[key]
+    data = data.assign(K=str(key))
+    df_list.append(data)
 
 # obtaining final data
 exp_data = pd.concat(df_list)
@@ -38,7 +40,7 @@ g = sns.FacetGrid(
     hue="K",
     palette=palette,
     margin_titles=True,
-    sharey = False
+    sharey=False,
 )
 g.map(plt.errorbar, "B", "MAE", "se", fmt="-o")
 g.add_legend()
@@ -46,21 +48,67 @@ g.add_legend()
 # savefigure
 plt.savefig(original_path + "/experiments/figures/exp_K_experiment.pdf")
 
+exp_data["K"] = exp_data["K"].astype(int)
+
+# Select the optimal K for each B and N in exp_data
+optimal_K = (
+    exp_data.groupby(["B", "N"])
+    .apply(lambda x: x.nsmallest(n=1, columns="MAE"))
+    .reset_index(drop=True)
+)
+
+
+# Plotting K for each B
+g = sns.FacetGrid(
+    optimal_K,
+    col="N",
+    col_wrap=2,
+    height=8,
+    aspect=1.60,
+    palette=palette,
+    margin_titles=True,
+    sharey=False,
+)
+
+g.map(sns.lineplot, "B", "K")
+plt.tight_layout()
+plt.savefig(original_path + "/experiments/figures/exp_optimal_K_versus_B.pdf")
+plt.show()
+
+# plotting each loss for B and K considering the optimal choosings of K
+g = sns.FacetGrid(
+    optimal_K,
+    col="N",
+    col_wrap=2,
+    height=8,
+    aspect=1.60,
+    palette=palette,
+    margin_titles=True,
+    sharey=False,
+)
+
+g.map(sns.lineplot, "B", "MAE")
+plt.tight_layout()
+plt.savefig(original_path + "/experiments/figures/exp_optimal_K_loss.pdf")
+plt.show()
+
+
+# --------------------------------------------------
 # bff experiment
+# --------------------------------------------------
+
 original_path = os.getcwd()
 data_path = original_path + "/experiments/results_data/experiment_K_bff.pkl"
-
 obj_exp = pd.read_pickle(data_path)
 df_list = []
 # creating data frame list to concatenate
 for key in obj_exp.keys():
-  data = obj_exp[key]
-  data = data.assign(K = str(key))
-  df_list.append(data)
+    data = obj_exp[key]
+    data = data.assign(K=str(key))
+    df_list.append(data)
 
 # obtaining final data
 bff_data = pd.concat(df_list)
-
 # obtaining graph
 sns.set(style="white", font_scale=1.5)
 g = sns.FacetGrid(
@@ -72,16 +120,62 @@ g = sns.FacetGrid(
     hue="K",
     palette=palette,
     margin_titles=True,
-    sharey = False
+    sharey=False,
 )
 g.map(plt.errorbar, "B", "MAE", "se", fmt="-o")
 g.add_legend()
-
 # savefigure
 plt.savefig(original_path + "/experiments/figures/bff_K_experiment.pdf")
 
 
+exp_data["K"] = exp_data["K"].astype(int)
+
+# Select the optimal K for each B and N in exp_data
+optimal_K = (
+    bff_data.groupby(["B", "N"])
+    .apply(lambda x: x.nsmallest(n=1, columns="MAE"))
+    .reset_index(drop=True)
+)
+
+
+# Plotting K for each B
+g = sns.FacetGrid(
+    optimal_K,
+    col="N",
+    col_wrap=2,
+    height=8,
+    aspect=1.60,
+    palette=palette,
+    margin_titles=True,
+    sharey=False,
+)
+
+g.map(sns.lineplot, "B", "K")
+plt.tight_layout()
+plt.savefig(original_path + "/experiments/figures/bff_optimal_K_versus_B.pdf")
+plt.show()
+
+# plotting each loss for B and K considering the optimal choosings of K
+g = sns.FacetGrid(
+    optimal_K,
+    col="N",
+    col_wrap=2,
+    height=8,
+    aspect=1.60,
+    palette=palette,
+    margin_titles=True,
+    sharey=False,
+)
+
+g.map(sns.lineplot, "B", "MAE")
+plt.tight_layout()
+plt.savefig(original_path + "/experiments/figures/bff_optimal_K_loss.pdf")
+plt.show()
+
+
+# --------------------------------------------------
 # gamma experiment
+# --------------------------------------------------
 original_path = os.getcwd()
 data_path = original_path + "/experiments/results_data/experiment_K_gamma.pkl"
 
@@ -89,9 +183,9 @@ obj_exp = pd.read_pickle(data_path)
 df_list = []
 # creating data frame list to concatenate
 for key in obj_exp.keys():
-  data = obj_exp[key]
-  data = data.assign(K = str(key))
-  df_list.append(data)
+    data = obj_exp[key]
+    data = data.assign(K=str(key))
+    df_list.append(data)
 
 # obtaining final data
 gamma_data = pd.concat(df_list)
@@ -107,7 +201,7 @@ g = sns.FacetGrid(
     hue="K",
     palette=palette,
     margin_titles=True,
-    sharey = False
+    sharey=False,
 )
 g.map(plt.errorbar, "B", "MAE", "se", fmt="-o")
 g.add_legend()
@@ -115,3 +209,46 @@ g.add_legend()
 # savefigure
 plt.savefig(original_path + "/experiments/figures/gamma_K_experiment.pdf")
 
+exp_data["K"] = exp_data["K"].astype(int)
+
+# Select the optimal K for each B and N in exp_data
+optimal_K = (
+    gamma_data.groupby(["B", "N"])
+    .apply(lambda x: x.nsmallest(n=1, columns="MAE"))
+    .reset_index(drop=True)
+)
+
+
+# Plotting K for each B
+g = sns.FacetGrid(
+    optimal_K,
+    col="N",
+    col_wrap=2,
+    height=8,
+    aspect=1.60,
+    palette=palette,
+    margin_titles=True,
+    sharey=False,
+)
+
+g.map(sns.lineplot, "B", "K")
+plt.tight_layout()
+plt.savefig(original_path + "/experiments/figures/gamma_optimal_K_versus_B.pdf")
+plt.show()
+
+# plotting each loss for B and K considering the optimal choosings of K
+g = sns.FacetGrid(
+    optimal_K,
+    col="N",
+    col_wrap=2,
+    height=8,
+    aspect=1.60,
+    palette=palette,
+    margin_titles=True,
+    sharey=False,
+)
+
+g.map(sns.lineplot, "B", "MAE")
+plt.tight_layout()
+plt.savefig(original_path + "/experiments/figures/gamma_optimal_K_loss.pdf")
+plt.show()
