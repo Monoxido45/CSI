e452e7f507dfb4c2b6089e20ba505b23f2a68a28
Monoxido45 <lucruz45.cab@gmail.com>
2024-04-10 14:20:47 -0300

    obtaining gmm results

--- experiments/plot_K_experiments.py
+++ experiments/plot_K_experiments.py
@@ -252,3 +252,85 @@ g.map(sns.lineplot, "B", "MAE")
 plt.tight_layout()
 plt.savefig(original_path + "/experiments/figures/gamma_optimal_K_loss.pdf")
 plt.show()
+
+# --------------------------------------------------
+# GMM experiment
+# --------------------------------------------------
+
+original_path = os.getcwd()
+data_path = original_path + "/experiments/results_data/gmm_experiments/cov_5000.pkl"
+
+obj_exp = pd.read_pickle(data_path)
+df_list = []
+# creating data frame list to concatenate
+for key in obj_exp.keys():
+    data = obj_exp[key]
+    data = data.assign(K=str(key))
+    df_list.append(data)
+
+# obtaining final data
+gamma_data = pd.concat(df_list)
+
+# obtaining graph
+sns.set(style="white", font_scale=1.5)
+g = sns.FacetGrid(
+    gamma_data,
+    col="N",
+    col_wrap=2,
+    height=8,
+    aspect=1.60,
+    hue="K",
+    palette=palette,
+    margin_titles=True,
+    sharey=False,
+)
+g.map(plt.errorbar, "B", "MAE", "se", fmt="-o")
+g.add_legend()
+
+# savefigure
+plt.savefig(original_path + "/experiments/figures/gmm_K_experiment.pdf")
+
+exp_data["K"] = exp_data["K"].astype(int)
+
+# Select the optimal K for each B and N in exp_data
+optimal_K = (
+    gamma_data.groupby(["B", "N"])
+    .apply(lambda x: x.nsmallest(n=1, columns="MAE"))
+    .reset_index(drop=True)
+)
+
+
+# Plotting K for each B
+g = sns.FacetGrid(
+    optimal_K,
+    col="N",
+    col_wrap=2,
+    height=8,
+    aspect=1.60,
+    palette=palette,
+    margin_titles=True,
+    sharey=False,
+)
+
+g.map(sns.lineplot, "B", "K")
+plt.tight_layout()
+plt.savefig(original_path + "/experiments/figures/gmm_optimal_K_versus_B.pdf")
+plt.show()
+
+# plotting each loss for B and K considering the optimal choosings of K
+g = sns.FacetGrid(
+    optimal_K,
+    col="N",
+    col_wrap=2,
+    height=8,
+    aspect=1.60,
+    palette=palette,
+    margin_titles=True,
+    sharey=False,
+)
+
+g.map(sns.lineplot, "B", "MAE")
+plt.tight_layout()
+plt.savefig(original_path + "/experiments/figures/gmm_optimal_K_loss.pdf")
+plt.show()
+
