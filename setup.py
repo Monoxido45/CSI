3f55fc621c8d3534bb66c548961f66e57cd69ce7
Monoxido45 <lucruz45.cab@gmail.com>
2024-07-26 17:47:25 -0300

    updating setup

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["numpy", "scikit-learn", "tqdm"],
+    install_requires=["numpy", "scikit-learn", "tqdm", "normflows", "torch"],
 )
