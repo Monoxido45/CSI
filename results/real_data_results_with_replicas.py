ac0d9883e1938530c1bd1d5f898b6efe2e77b60b
guipsoares <guilherme_soares@usp.br>
2024-08-08 23:17:55 -0300

    edit file to run comparisons with already sampled data

--- results/real_data_results_with_replicas.py
+++ results/real_data_results_with_replicas.py
@@ -3,15 +3,21 @@ import pandas as pd
 import torch
 from tqdm import tqdm
 import os
-from CP2LFI.utils import obtain_quantiles
+from CP2LFI.utils import obtain_quantiles, CPU_Unpickler
+
+from hypothesis.benchmark import sir, tractable, mg1, weinberg
+import sbibm
+
+import itertools
 
 # general path
 original_path = os.getcwd()
+stats_path = "/results/LFI_objects/data/"
 
 
 def compute_MAE_N_B(
     kind,
-    score,
+    score_name,
     theta_grid_eval,
     simulator,
     prior,
@@ -44,9 +50,13 @@ def compute_MAE_N_B(
     # third column: TRUST++ tuned
     # fourth column: boosting
     # fifth column: monte-carlo
+    with open(original_path + stats_path + f"{kind}_{score_name}_{N}.pickle", "rb") as f:
+        score = CPU_Unpickler(f).load()
+
+    score.base_model.model.device = torch.device("cpu")
 
     # creating folder if not existing
-    var_path = kind + "/" + score
+    var_path = kind + "/" + score_name
     save_path = original_path + folder_path + var_path
 
     if not os.path.isdir(original_path + folder_path + var_path):
@@ -89,6 +99,7 @@ def compute_MAE_N_B(
                 theta_repeated = torch.tensor([theta]).repeat_interleave(
                     repeats=n_lambda * N, dim=0
                 )
+                theta = tuple(theta)
 
             # simulating lambdas for testing
             X_net = simulator(theta_repeated)
@@ -97,9 +108,12 @@ def compute_MAE_N_B(
             X_dim = X_net.shape[1]
             X_net = X_net.reshape(n_lambda, N * X_dim)
 
-            stat = score.compute(
-                theta_repeated.numpy()[0:n_lambda, :], X_net.numpy(), disable_tqdm=True
-            )
+            # stat = score.compute(
+            #     theta_repeated.numpy()[0:n_lambda, :], X_net.numpy(), disable_tqdm=True
+            # )
+            with open(original_path + folder_path + f"{kind}_{score_name}_eval_{n}.pickle", "rb") as f:
+                stat = CPU_Unpickler(f).load()
+            stat = stat.get(theta)
 
             # comparing coverage of methods
             locart_cover = np.mean(stat <= quantiles_dict["locart"][l])
@@ -156,11 +170,62 @@ if __name__ == "__main__":
     it = int(input("How many iterations? "))
     print(f"Starting experiment for {kind} simulator with {score} statistic")
 
-    # creating
+    two_moons = False
+    # importing simulator and prior for each kind of statistic
+    if kind == "weinberg":
+        simulator = weinberg.Simulator(default_beam_energy=40.0)
+        prior = weinberg.Prior()
 
+        n_out = 300
+        thetas_valid = np.linspace(0.51, 1.49, n_out)
+    elif kind == "sir":
+        simulator = sir.Simulator()
+        prior = sir.Prior()
+
+        n_par = 22
+        pars_1 = np.linspace(0.01, 0.49, n_par)
+        thetas_valid = np.c_[list(itertools.product(pars_1, pars_1))]
+    elif kind == "tractable":
+        simulator = tractable.Simulator()
+        prior = tractable.Prior()
+
+        n_par = 5
+        pars = np.linspace(-2.9, 2.9, n_par)
+        pars[pars == 0] = 0.01
+        thetas_valid = np.c_[list(itertools.product(pars, pars, pars, pars, pars))]
+    elif kind == "mg1":
+        simulator = mg1.MG1Simulator()
+        prior = mg1.Prior()
+
+        n_par = 8
+        pars_1 = np.linspace(0.1, 9.9, n_par)
+        pars_2 = np.linspace(0.01, 1 / 3 - 0.01, n_par)
+        thetas_valid = np.c_[list(itertools.product(pars_1, pars_1, pars_2))]
+        log_transf = True
+    elif kind == "two moons":
+        task = sbibm.get_task(
+            "two_moons"
+        )  # See sbibm.get_available_tasks() for all tasks
+        simulator = task.get_simulator()
+        prior = task.get_prior()
+        two_moons = True
+
+        n_par = 20
+        pars_1 = np.linspace(-0.99, 0.99, n_par)
+        thetas_valid = np.c_[list(itertools.product(pars_1, pars_1))]
+
+    # creating
     for n in n_list:
         # TODO: open already fitted statistic with posterior estimator to avoid too much time fitting all
         # also, save the tuning samples for TRUST++ in each case
         for B in B_list:
             # fill in with what remains.
-            compute_MAE_N_B()
+            compute_MAE_N_B(
+                kind,
+                score,
+                thetas_valid,
+                simulator,
+                prior,
+                N=n,
+                B=B,
+            )
